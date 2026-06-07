426fcd7af6ead0c88084c89aa4d8fc277f8444a7
Erik Berg <erik@erikberg.se>
2026-06-08 00:49:26 +0200

    index

--- backups/config.js
+++
@@ -1,4 +0,0 @@
-const SUPA_URL = 'https://wtsaivdajjiyymvwuksc.supabase.co';
-const SUPA_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0c2FpdmRhamppeXltdnd1a3NjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyNDg5NjQsImV4cCI6MjA5MTgyNDk2NH0.rHuzK0Siov-Viy1cseVJYn-8xiNSjOCJKxUquurXs2M';
-
-const sb = supabase.createClient(SUPA_URL, SUPA_KEY);
